1cef9d8571031227a418aba3b5f0794d5533bd92
unknown <keat@APPLIED.COM.MY>
2018-11-02 19:22:47 +0800

    modification

--- src/main/java/ai/api/examples/web/WebhookSampleV1.java
+++ src/main/java/ai/api/examples/web/WebhookSampleV1.java
@@ -1,23 +1,242 @@
 package ai.api.examples.web;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.servlet.annotation.WebServlet;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
-import ai.api.model.AIRequest;
+import ai.api.model.AIOutputContext;
 import ai.api.model.Fulfillment;
 import ai.api.web.AIWebhookServletV1;
 
 @WebServlet("/webhookV1")
 public class WebhookSampleV1 extends AIWebhookServletV1 {
-  private static final long serialVersionUID = 1L;
+	
+	private static final long serialVersionUID = 1L;
+	private final String  webServiceURL = "http://localhost/th/webservice_json";
+	
+	@Override
+	protected void doWebhook(AIWebhookRequest input, Fulfillment output, boolean testMode) {
+		try {
+			if ( testMode ) {
+				postToStub(generateRequestJSON(input),output);
+			} else {
+				postToWebservice(generateRequestJSON(input),output);
+			}
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
-  @Override
-  protected void doWebhook( ai.api.model.AIRequest input, Fulfillment output) {
-	  output.setSpeech("OUTPUT : " + input.getSessionId());
-	  // this is to call internal URL
-	  // parameters are getting from input
-  }
+	private String generateRequestJSON(AIWebhookRequest input) throws JsonProcessingException {
+		
+			ObjectMapper mapper = new ObjectMapper();
+			String dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+			mapper.setDateFormat(new SimpleDateFormat(dateFormat));
+			HashMap<String, JsonElement> params = input.getResult().getParameters();
+			
+			Map<String, Object> jsonMap = new HashMap<String, Object>();
+			Map<String, Object> inputMap = new HashMap<String, Object>();
+		
+			jsonMap.put("action_name", input.getResult().getMetadata().getIntentName());
+			
+			Iterator<Entry<String, JsonElement>> it = params.entrySet().iterator();
+		
+			String key = null;
+			JsonElement value = null;
+			while (it.hasNext()) {
+			        Entry<String, JsonElement> pair = it.next();
+			        key = pair.getKey();   		
+			        
+			        if ( ("access_key_id").equals(key) || "timestamp".equals(key) ||
+			        		"signature".equals(key) ) {
+			        	jsonMap.put((String) pair.getKey(),pair.getValue().getAsString());
+			        } else {
+			        	inputMap.put((String) pair.getKey(),pair.getValue().getAsString());
+			        }
+			        it.remove();
+			}
+			jsonMap.put("input",inputMap);
+			
+			/*{
+				  "action_name": "leave.list_leave_entitlement",
+				  "access_key_id": "8NvQ92ckO2yXP5qCDmrz",
+				  "timestamp": "02/11/2018 00:00:00.0",
+				  "signature": "UGrR9+J0p+2g63PQRF5Hru2/0eI=",
+				  "tokenid" : "511c781a-85a7-4f40-80e5-49a1c64dc51b",
+				  "input": {
+				    "zyear": 2017
+				  }
+			}*/
+			/*{
+				  "action_name": "login",
+				  "access_key_id":"8NvQ92ckO2yXP5qCDmrz",
+				  "timestamp":"",
+				  "signature":"dUw0ZJXqjBN75Wxd0HfdVITNflY=",
+				  "input": {
+				    "username": "ahmad.arif",
+				    "password": "TstVnPgIHe1pvJDMK0V6Pw=="
+				  
+				  }
+				}*/
 
+			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonMap);
+	}
+	
+	private void postToWebservice(String data,Fulfillment output) {
+		
+		HttpClient httpClient = HttpClientBuilder.create().build(); 
+	    
+		try {
+			HttpPost request = new HttpPost(webServiceURL);
+		    StringEntity params =new StringEntity(data);
+		    request.addHeader("content-type",  "application/json");
+		    request.setEntity(params);
+		    generateResponse( httpClient.execute(request),output);
+		}catch (Exception ex) {
+			//handle exception here
+		} finally {
+			httpClient.getConnectionManager().shutdown(); 
+		}
+		
+	}
+	
+	private void postToStub(String data, Fulfillment output)  {
 
-  
-  
+		JsonNode jsonInput = null;
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			jsonInput = mapper.readTree(data);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return;
+		}
+		
+		String response = "";
+		if (jsonInput.path("action_name").asText().equals("login")) {
+			response = "{\"results\": {\"stafftype\": 1,\"gender\": 1,\"tokenid\": \"511c781a-85a7-4f40-80e5-49a1c64dc51b\",\"staffname\": \"AHMAD ARIF BIN ABDUL AZIZ\"},\"status\": \"success\",\"success_msg\": \"Login successful\"}";
+		} else if (jsonInput.path("action_name").asText().equals("leave.list_leave_entitlement")) {
+			response = "{\"results\":{ \"RH02\":{ \"balance\":-6, \"leavetypedesc\":\"CUTIKECEMASAN\", \"leavetypecode\":\"RH02\", \"showbalance\":0 }, \"BK01\":{ \"balance\":-1, \"leavetypedesc\":\"CUTIGANTIAN-LEBIHMASA\", \"leavetypecode\":\"BK01\", \"showbalance\":1 }, \"TR02\":{ \"balance\":-3, \"leavetypedesc\":\"CUTITANPAREKOD-PEPERIKSAAN/BELAJAR\", \"leavetypecode\":\"TR02\", \"showbalance\":0 } }, \"status\":\"success\", \"success_msg\":\"\" }";
+		}
+		generateStubResponse(response,output);
+	}
+	
+
+	private void generateResponse(HttpResponse response, Fulfillment output) throws IOException {
+		
+		JsonNode jsonResp = null;
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			jsonResp = mapper.readTree(getBody(response));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		//	handleError(request, response, "Param is invalid");
+			return;
+		}
+		output.setDisplayText(jsonResp.path("success_msg").asText());
+		output.setContextOut(prepareOutputContext(jsonResp));
+		
+	}
+
+	private void generateStubResponse(String jsonData, Fulfillment output)  {
+		
+		JsonNode jsonResp = null;
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			jsonResp = mapper.readTree(jsonData);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		//	handleError(request, response, "Param is invalid");
+			return;
+		}
+		output.setDisplayText(jsonResp.path("success_msg").asText());
+		output.setContextOut(prepareOutputContext(jsonResp));
+		
+	}
+	
+	private AIOutputContext prepareOutputContext(JsonNode jsonResp) {
+		
+		AIOutputContext contextOut = new AIOutputContext();
+		contextOut.setName("login-authenticated");
+		contextOut.setLifespan(new Integer(500));
+		
+		try {
+		Gson gson = new Gson();
+		Iterator<String> nodes = jsonResp.path("results").fieldNames();
+		Map<String, JsonElement> tokenParam = new HashMap();
+		while ( nodes.hasNext()) {
+			String node = nodes.next();
+			String value = jsonResp.path("results").path(node).asText();
+			if ( value.contains(" ")) {
+				value = '"' + value + '"';
+			}
+			JsonElement element = gson.fromJson(value, JsonElement.class);
+			tokenParam.put(node, element);
+		}
+		contextOut.setParameters(tokenParam);
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		return contextOut;
+	}
+	
+	private static String getBody(HttpResponse response) throws IOException {
+
+	    String body = null;
+	    StringBuilder stringBuilder = new StringBuilder();
+	    BufferedReader bufferedReader = null;
+
+	    InputStream inputStream = response.getEntity().getContent();
+	    
+	    try {
+	    
+	        if (inputStream != null) {
+	            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	            char[] charBuffer = new char[128];
+	            int bytesRead = -1;
+	            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+	                stringBuilder.append(charBuffer, 0, bytesRead);
+	            }
+	        } else {
+	            stringBuilder.append("");
+	        }
+	    } catch (IOException ex) {
+	        throw ex;
+	    } finally {
+	        if (bufferedReader != null) {
+	            try {
+	                bufferedReader.close();
+	            } catch (IOException ex) {
+	                throw ex;
+	            }
+	        }
+	    }
+	    body = stringBuilder.toString();
+	    return body;
+	}	
+
+	//
+	//	below are for backend offline testing
+	//
+	
+
 }
